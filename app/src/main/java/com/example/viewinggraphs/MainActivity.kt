016150ee77dd66743c8448b868ebb9379a14dadb
Apocalixs-Nick <nicolapiccirillo04@gmail.com>
2023-01-24 16:04:47 +0100

    Removed select button and add 3 button on toolbar for switching graphs

--- app/src/main/java/com/example/viewinggraphs/MainActivity.kt
+++ app/src/main/java/com/example/viewinggraphs/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.viewinggraphs.databinding.ActivityMainBinding
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var selectVisible = false
+    //private var selectVisible = false
 
     private lateinit var navController: NavController
 
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.root.isNestedScrollingEnabled = false
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main)
-        findViewById<View>(R.id.select1).setOnClickListener {
+        /*findViewById<View>(R.id.select1).setOnClickListener {
             navController.navigate(R.id.barChartFragment)
         }
         findViewById<View>(R.id.select2).setOnClickListener {
@@ -32,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         }
         findViewById<View>(R.id.select3).setOnClickListener {
             navController.navigate(R.id.candleStickChartFragment)
-        }
+        }*/
 
     }
 
@@ -43,18 +44,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId){
-            R.id.settings -> {
+            /*R.id.setting -> {
                 selectVisible = !selectVisible
                 if (selectVisible) {
                     findViewById<View>(R.id.select1).visibility = View.VISIBLE
                     findViewById<View>(R.id.select2).visibility = View.VISIBLE
                     findViewById<View>(R.id.select3).visibility = View.VISIBLE
+                    findViewById<Button>(R.id.select1).setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_bar, 0, 0, 0)
+                    findViewById<Button>(R.id.select2).setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_pie, 0, 0, 0)
+                    findViewById<Button>(R.id.select3).setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_candle, 0, 0, 0)
                 } else {
                     findViewById<View>(R.id.select1).visibility = View.GONE
                     findViewById<View>(R.id.select2).visibility = View.GONE
                     findViewById<View>(R.id.select3).visibility = View.GONE
+                    findViewById<Button>(R.id.select1).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                    findViewById<Button>(R.id.select2).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                    findViewById<Button>(R.id.select3).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
                 }
-            }
+            }*/
+            R.id.select1 -> navController.navigate(R.id.barChartFragment)
+            R.id.select2 -> navController.navigate(R.id.pieChartFragment)
+            R.id.select3 ->navController.navigate(R.id.candleStickChartFragment)
         }
         return super.onOptionsItemSelected(item)
     }
